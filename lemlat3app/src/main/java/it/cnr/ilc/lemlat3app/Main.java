3ea3acb799e46ab6a200acf6dae8e951db63e00e
riccardodg <riccardodelgratta@192.168.1.15>
2017-03-06 17:34:12 +0100

    Included lemlat3 utils added read properties in main

--- lemlat3app/src/main/java/it/cnr/ilc/lemlat3app/Main.java
+++ lemlat3app/src/main/java/it/cnr/ilc/lemlat3app/Main.java
@@ -1,32 +1,44 @@
 package it.cnr.ilc.lemlat3app;
 
-
+import it.cnr.ilc.lemlat3utils.PropertyLoader;
 import java.util.Properties;
-
-
-
+import org.apache.log4j.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
- *
+ * Main class, just to play around
  * @author riccardodelgratta
  */
 public class Main {
+
+    static final String CLASS_NAME = Main.class.getName();
+
+    static Logger log = Logger.getLogger(CLASS_NAME);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Main.main()");
-        PropertyLoader propLoad=new PropertyLoader("/db.properties");
-       Properties prop = propLoad.propertyLoader("/db.properties");
-       String host=prop.getProperty("host");
-       System.err.println("host "+host);
+        String routine = "main routine";
+        String logmess = "a";
+        
+        PropertyLoader propLoad = new PropertyLoader("/lemlat3.properties");
+        Properties props = propLoad.propertyLoader();
+        String host = propLoad.getPropertyAsString(props, "hosta");
+
+        boolean deb = propLoad.getPropertyAsBoolean(props, "debug");
+        if (deb) {
+            System.out.println("Main.main()");
+            logmess = String.format("Property 'host ' with value -%s- %s", host, routine);
+            log.info(logmess);
+            logmess = String.format("Property 'deb ' with value -%s- %s", deb, routine);
+            log.info(logmess);
+        }
+
     }
-    
+
 }
